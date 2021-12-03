913f087585e132f831b55cbb80012a56086b5892
luong210978 <luong20001117@gmail.com>
2021-12-03 10:40:49 +0700

    Revert "Revert "Revert "update to revert"""
    This reverts commit f8b0a935fc9af60e7df9b5e309ae5a844cb93432.

--- android/app/src/main/java/com/homeworkout/MainApplication.java
+++ android/app/src/main/java/com/homeworkout/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
