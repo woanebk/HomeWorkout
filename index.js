5129a19f4f70c5d71d028738e9fd629773f79a9e
luong210978 <luong20001117@gmail.com>
2021-12-07 17:06:36 +0700

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
+
   console.log('Message handled in the background!', remoteMessage);
 });
 AppRegistry.registerComponent(appName, () => App);
