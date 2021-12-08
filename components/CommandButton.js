2108e1c6a95f43c174e3a5289d46f8462ee28e42
DESKTOP-R63FP6R\HTComputer <nam.dao@kyanon.digital>
2021-12-08 22:50:40 +0700

    update challenge detail screen

--- components/CommandButton.js
+++ components/CommandButton.js
@@ -9,7 +9,7 @@ function CommandButton(props) {
       <TouchableOpacity
         style={[
           styles.commandBtn,
-          props.backgroundColor? {backgroundColor: props.backgroundColor} : {}
+          props.backgroundColor ? {backgroundColor: props.backgroundColor} : {},
         ]}
         onPress={props.onPress}>
         <Icon
@@ -18,22 +18,54 @@ function CommandButton(props) {
           size={props.iconSize ? props.iconSize : 13}
           color={props.iconColor ? props.iconColor : COLOR.WHITE}
         />
-        <Text style={props.textStyle ? props.textStyle : {color:props.textColor?textColor:COLOR.WHITE, fontSize:15, fontWeight:'bold', marginLeft:10}}>{props.title}</Text>
+        <Text
+          style={
+            props.textStyle
+              ? props.textStyle
+              : {
+                  color: props.textColor ? textColor : COLOR.WHITE,
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                  marginLeft: 10,
+                }
+          }>
+          {props.title}
+        </Text>
+        {props.hasRightIcon && (
+          <View
+            style={[, props.rightIconContainerStyle, styles.rightIconWrapper]}>
+            <Icon
+              name={props?.rightIcon || 'arrow-circle-right'}
+              type="font-awesome"
+              size={props?.rightIconSize || 30}
+              color={props?.rightIconColor || COLOR.WHITE}
+            />
+          </View>
+        )}
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    commandBtn: {
+  commandBtn: {
     backgroundColor: COLOR.BLUE,
     borderRadius: 7,
     justifyContent: 'center',
-    alignItems:'center',
-    flexDirection:'row',
-    width:'100%',
-    height:'100%'
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    height: '100%',
   },
+  rightIconWrapper: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rightIcon: {},
 });
 
 export default CommandButton;
---
+++ components/WorkoutByDayItem.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+} from 'react-native';
+import {COLOR} from '../constant';
+import {Icon} from 'react-native-elements';
+
+function WorkoutByDayItem(props) {
+  return (
+    <View style={props?.style}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View>
+          <Text numberOfLines={1} style={styles.dayTxt}>
+            Ngày 1
+          </Text>
+          <Text numberOfLines={1} style={styles.day2Txt}>
+            Vai, Ngực
+          </Text>
+        </View>
+        {props?.isDone && (
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginRight: 30,
+            }}>
+            <Icon
+              name="check-circle"
+              type="font-awesome-5"
+              size={23}
+              color={COLOR.WHITE}
+            />
+          </View>
+        )}
+      </View>
+      <TouchableOpacity onPress={props?.onPress}>
+        <View style={[props.style, styles.container]}>
+          {props?.isDone ? (
+            <ImageBackground
+              style={styles.imageBackgroundDone}
+              imageStyle={[props?.imageStyle, styles.image, {opacity: 0.4}]}
+              source={props?.image}
+              resizeMode="cover">
+              <Text
+                numberOfLines={1}
+                style={[styles.titleTxt, {fontWeight: 'bold'}]}>
+                Đã Hoàn Thành
+              </Text>
+            </ImageBackground>
+          ) : (
+            <ImageBackground
+              style={styles.imageBackground}
+              imageStyle={[props?.imageStyle, styles.image]}
+              source={props?.image}
+              resizeMode="cover">
+              <Text numberOfLines={1} style={styles.titleTxt}>
+                {props.title}
+              </Text>
+            </ImageBackground>
+          )}
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 160,
+    height: 70,
+    backgroundColor: '#000',
+    borderRadius: 7,
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  imageBackgroundDone: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 7,
+  },
+  titleTxt: {
+    color: COLOR.WHITE,
+    fontSize: 16,
+    marginHorizontal: 5,
+    marginBottom: 5,
+  },
+  desTxt: {
+    color: COLOR.WHITE,
+    fontSize: 12,
+  },
+  dayTxt: {
+    marginTop: 10,
+    marginBottom: 5,
+    fontSize: 15,
+    color: COLOR.WHITE,
+    fontWeight: 'bold',
+  },
+  day2Txt: {
+    marginBottom: 5,
+    fontSize: 13,
+    color: COLOR.WHITE,
+  },
+});
+
+export default WorkoutByDayItem;
