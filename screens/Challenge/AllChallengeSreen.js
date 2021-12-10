965442d67615766ab68368208d6833e7f27760c1
luong210978 <luong20001117@gmail.com>
2021-12-10 15:58:00 +0700

    update

--- screens/Challenge/AllChallengeSreen.js
+++ screens/Challenge/AllChallengeSreen.js
@@ -6,164 +6,26 @@ import {
     View,
     StyleSheet,
     useColorScheme,
-    FlatList,
+    FlatList,TouchableOpacity
   } from 'react-native';
   import {COLOR} from '../../constant';
   import MasonryList from '@react-native-seoul/masonry-list';
   import React, {FC, ReactElement, useMemo, useState, useEffect} from 'react';
   
-  function AllChallengeScreen() {
-    const DATA = [
-      {
-        id: 'id123',
-        imgURL:
-          'https://ii1.pepperfry.com/media/catalog/product/m/o/568x625/modern-chaise-lounger-in-grey-colour-by-dreamzz-furniture-modern-chaise-lounger-in-grey-colour-by-dr-tmnirx.jpg',
-        text: 'Pioneer LHS Chaise Lounger in Grey Colour',
-      },
-      {
-        id: 'id124',
-        imgURL:
-          'https://www.precedent-furniture.com/sites/precedent-furniture.com/files/styles/header_slideshow/public/3360_SL%20CR.jpg?itok=3Ltk6red',
-        text: 'Precedant Furniture',
-      },
-      {
-        id: 'id125',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/leverette-fabric-queen-upholstered-platform-bed-1594829293.jpg',
-        text: 'Leverette Upholstered Platform Bed',
-      },
-      {
-        id: 'id126',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/briget-side-table-1582143245.jpg?crop=1.00xw:0.770xh;0,0.129xh&resize=768:*',
-        text: 'Briget Accent Table',
-      },
-      {
-        id: 'id127',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rivet-emerly-media-console-1610578756.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Rivet Emerly Media Console',
-      },
-      {
-        id: 'id128',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/drew-barrymore-flower-home-petal-chair-1594829759.jpeg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Drew Barrymore Flower Home Accent Chair',
-      },
-      {
-        id: 'id129',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goodee-ecobirdy-charlie-chairs-1594834221.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Ecobirdy Charlie Chair',
-      },
-      {
-        id: 'id130',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hailey-sofa-1571430947.jpg?crop=0.481xw:0.722xh;0.252xw,0.173xh&resize=768:*',
-        text: 'Hailey Sofa',
-      },
-      {
-        id: 'id131',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/archer-home-designs-dining-table-1594830125.jpg?crop=0.657xw:1.00xh;0.0986xw,0&resize=768:*',
-        text: 'Farmhouse Dining Table',
-      },
-      {
-        id: 'id132',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/evelyn-coffee-table-1610578857.jpeg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Evelyn Coffee Table',
-      },
-      {
-        id: 'id133',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/burrow-nomad-sofa-1594837995.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Slope Nomad Leather Sofa',
-      },
-      {
-        id: 'id134',
-        imgURL:
-          'https://apicms.thestar.com.my/uploads/images/2020/02/21/570850.jpg',
-        text: 'Chair and Table',
-      },
-      {
-        id: 'id223',
-        imgURL:
-          'https://ii1.pepperfry.com/media/catalog/product/m/o/568x625/modern-chaise-lounger-in-grey-colour-by-dreamzz-furniture-modern-chaise-lounger-in-grey-colour-by-dr-tmnirx.jpg',
-        text: 'Pioneer LHS Chaise Lounger in Grey Colour',
-      },
-      {
-        id: 'id224',
-        imgURL:
-          'https://www.precedent-furniture.com/sites/precedent-furniture.com/files/styles/header_slideshow/public/3360_SL%20CR.jpg?itok=3Ltk6red',
-        text: 'Precedant Furniture',
-      },
-      {
-        id: 'id225',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/leverette-fabric-queen-upholstered-platform-bed-1594829293.jpg',
-        text: 'Leverette Upholstered Platform Bed',
-      },
-      {
-        id: 'id226',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/briget-side-table-1582143245.jpg?crop=1.00xw:0.770xh;0,0.129xh&resize=768:*',
-        text: 'Briget Accent Table',
-      },
-      {
-        id: 'id227',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rivet-emerly-media-console-1610578756.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Rivet Emerly Media Console',
-      },
-      {
-        id: 'id228',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/drew-barrymore-flower-home-petal-chair-1594829759.jpeg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Drew Barrymore Flower Home Accent Chair',
-      },
-      {
-        id: 'id229',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goodee-ecobirdy-charlie-chairs-1594834221.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Ecobirdy Charlie Chair',
-      },
-      {
-        id: 'id230',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hailey-sofa-1571430947.jpg?crop=0.481xw:0.722xh;0.252xw,0.173xh&resize=768:*',
-        text: 'Hailey Sofa',
-      },
-      {
-        id: 'id231',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/archer-home-designs-dining-table-1594830125.jpg?crop=0.657xw:1.00xh;0.0986xw,0&resize=768:*',
-        text: 'Farmhouse Dining Table',
-      },
-      {
-        id: 'id232',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/evelyn-coffee-table-1610578857.jpeg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Evelyn Coffee Table',
-      },
-      {
-        id: 'id233',
-        imgURL:
-          'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/burrow-nomad-sofa-1594837995.jpg?crop=1xw:1xh;center,top&resize=768:*',
-        text: 'Slope Nomad Leather Sofa',
-      },
-      {
-        id: 'id234',
-        imgURL:
-          'https://apicms.thestar.com.my/uploads/images/2020/02/21/570850.jpg',
-        text: 'Chair and Table',
-      },
-  
-    ];
-    var [allChallenges, setAllChallenges] = useState(DATA);
-    // useEffect(() => {
-    //     // setAllChallenges(DATA);
-    // });
+  function AllChallengeScreen({route,navigation}) {
+    const { type,challenges} = route.params;
+
+    const DATA = [{
+      id: undefined,
+      imgURL:
+      undefined,
+    },];
+    var [allChallenges, setAllChallenges] = useState(challenges);
+    useEffect(() => {
+       console.log(challenges)
+       console.log(type)
+
+    });
     var FurnitureCard = function (_a) {
       var item = _a.item;
       var randomBool = (0, useMemo)(function () {
@@ -171,23 +33,24 @@ import {
       }, []);
       return (
         <View key={item.id} style={{marginTop: 12,marginLeft:12, flex: 1,}}>
+          <TouchableOpacity onPress={()=>{ navigation.navigate("ChallengeDetail",{item})}}>
           <Image
             source={{uri:item.imgURL} }
             style={{
-              height: randomBool ? 150 : 280,
+              height: randomBool ? 240 : 280,
               alignSelf: 'stretch',
               borderRadius:20,
   
             }}
-  
             resizeMode="cover"
           />
           <Text
             style={{
              color:COLOR.WHITE,marginTop:5
             }}>
-            {item.text}
+            {item.title}
           </Text>
+          </TouchableOpacity>
         </View>
       );
     };
@@ -197,15 +60,30 @@ import {
     };
     return (
       <View style={{flex: 1, backgroundColor: COLOR.MATTE_BLACK}}>
+       <View style={{flexDirection: 'row'}}>
+        <Text
+          style={{
+            marginTop: 60,
+            color: COLOR.MATTE_BLACK,
+            fontSize: 20,
+            marginLeft: 0,
+            marginBottom: 10,
+            width: 200,
+            borderBottomRightRadius: 20,
+            borderTopRightRadius: 20,
+
+            paddingLeft: 10,
+            backgroundColor: COLOR.GOLD,
+          }}>
+          {type=="All"?"Tất cả thử thách":"Thử thách của tôi"}
+        </Text>
+       
+      </View>
         <StatusBar backgroundColor="transparent" translucent style={{height:50}} />
+        
           <MasonryList
             ListHeaderComponent={<View  style={{flex: 1,marginTop:0}} >
-            <Text
-            style={{
-              marginTop: 50,color:COLOR.WHITE,fontSize:20,color:COLOR.WHITE
-            }}>
-             Danh sách thử thách
-          </Text> 
+          
           </View>}
             contentContainerStyle={{
               marginLeft: 12,
