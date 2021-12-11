0a0475ff4145f39e4632074300a1a576ad96ae8d
DESKTOP-R63FP6R\HTComputer <nam.dao@kyanon.digital>
2021-12-11 17:52:14 +0700

    done all excercise screen

---
+++ components/LoadingView.js
@@ -0,0 +1,28 @@
+import React, {forwardRef, useEffect, useRef, useState} from 'react';
+import {Text, StyleSheet, View, Modal} from 'react-native';
+import LottieView from 'lottie-react-native';
+import { COLOR, SCREEN_HEIGHT } from '../constant';
+
+function LoadingView(props) {
+  
+
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <LottieView
+        style={styles.animation}
+        source={require('../assets/lottie/loading.json')}
+        autoPlay
+        loop></LottieView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    animation:{
+        width:100,
+        height:100,
+        alignSelf:'center'
+    },
+});
+
+export default LoadingView;
